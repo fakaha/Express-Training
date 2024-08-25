14747dafb5258badcd09aa24ff137e9855972545
fakaha <lunastra34@gmail.com>
2024-08-26 01:15:05 +0700

    feat: add connection to database

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
-const express = require('express')
+const express = require("express");
+const mysql = require("mysql2");
 
-const usersRoutes = require('./routes/users')
+const usersRoutes = require("./routes/users");
 
-const middlewareLogRequest = require('./middleware/logs')
+const middlewareLogRequest = require("./middleware/logs");
 
-const app = express()
+const dbPool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "express_mysql",
+  port: 3307,
+});
 
-app.use(middlewareLogRequest)
+const app = express();
+
+app.use(middlewareLogRequest);
 // mengizinkan semua request json
-app.use(express.json())
+app.use(express.json());
 
 // semua path users yang sudah dibuat akan masuk di sini
-app.use('/users', usersRoutes)
+app.use("/users", usersRoutes);
 
+app.use("/", (req, res) => {
+  dbPool.execute(`SELECT * FROM users`, (err, rows) => {
+    if (err === true) {
+      res.json({
+        message: "connection failed",
+      });
+    }
+    res.json({
+      message: "connection success",
+      data: rows,
+    });
+  });
+});
 
 app.listen(4000, () => {
-    console.log(`Server berhasil running di port 4000`);
-})
+  console.log(`Server berhasil running di port 4000`);
+});
